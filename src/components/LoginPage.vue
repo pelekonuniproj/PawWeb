dda181eba6231031f196daef01b2e2d10e28c7b6
Kamila <kami.wiewiora@gmail.com>
2019-11-26 23:18:11 +0100

    dodanie widoku logowania

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="row mt-5">
+        <div class="col-4 offset-4">
+            <form>
+                <div class="form-group">
+                    <label class="my-label" for="inputLogin">Login</label>
+                    <input type="text" class="form-control" id="inputLogin" placeholder="Wpisz swój login">
+                </div>
+                <div class="form-group">
+                    <label class="my-label" for="inputPassword">Hasło</label>
+                    <input type="password" class="form-control" id="inputPassword" placeholder="Wpisz swoje hasło">
+                </div>
+                <button type="submit" class="btn btn-info my-dark-button">Zaloguj</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'LoginPage',
+        components: {
+        },
+
+        data: () => ({
+
+        }),
+
+        methods: {
+
+        },
+    }
+</script>
