79fed30c897d73c75a5c159ef281711106b758e0
Kamila <kami.wiewiora@gmail.com>
2020-01-15 14:08:22 +0100

    reformat klasy BoardSetion (task -> card)

--- src/components/Board/Lists/BoardSection.vue
+++ src/components/Board/Lists/BoardSection.vue
@@ -8,8 +8,8 @@
             @showMoveAllCards="handleMoveAllCards" @showSort="handleSortAllCards" @showMoveList="handleShowMoveList"/>
         </div>
         <component v-bind:is="componentName" @show="close"/>
-        <BoardCard class="d-flex flex-column board-section-task-holder" v-for="task in tasks" v-bind:key="task.id"
-                    v-bind:name="task.title" v-bind:id="task.id" v-bind:desc="task.description" />
+        <BoardCard class="d-flex flex-column board-section-task-holder" v-for="card in cards" v-bind:key="card.id"
+                   v-bind:name="card.title" v-bind:id="card.id" v-bind:desc="card.description" />
         <AddCard/>
     </div>
 </template>
@@ -40,19 +40,19 @@ import AddCard from "./AddCard";
             BoardCard,
         },
         
-        props: ['sectionName', 'tasksList', 'id'],
+        props: ['sectionName', 'cardsList', 'id'],
         
         data: function () {
             return {
                 componentName: null,
-                tasks: []
+                cards: []
             }
         },
         
         mounted() {
             var self = this
             ApiClient.getCardsForBoardSection(this.id, function(response) {
-                self.tasks = response
+                self.cards = response
             })
         },
         
