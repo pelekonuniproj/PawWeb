cf8022c16e0d020ec1722a62a95dc9e8392a70bb
Kamila <kami.wiewiora@gmail.com>
2020-01-16 13:02:09 +0100

    dodanie półprzezroczystego tła, gdy mamy włączone szzegóły karty

--- src/components/Board/Cards/CardDetails.vue
+++ src/components/Board/Cards/CardDetails.vue
@@ -1,75 +1,78 @@
 <template>
-    <div class="modal details-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">{{ cardName }}</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close" v-on:click="closeDetails">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-sm">
-                        <div class="form-group">
-                            <h5 class="title">Listy zadań</h5>
-                            <div class="row">
-                                <div class="col-10">
-                                    <input v-model="newListName" type="text" class="form-control input-style"
-                                           placeholder="Dodaj nową listę zadań...">
-                                </div>
-                                <div class="col-0">
-                                    <button type="button" v-on:click="addNewTaskList"
-                                            class="btn btn-info btn-sm my-button">Dodaj
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <TasksList class="form-group" v-for="taskList in taskLists" v-bind:key="taskList.id"
-                                       v-bind:id="taskList.id"
-                                       v-bind:name="taskList.name" v-bind:taskJsons="taskList.taskJsons"
-                                       @refresh-window="downloadTasks"></TasksList>
-                        </div>
-                    </div>
-                    <div class="col-sm">
-                        <form id="comment-form">
+    <div class="my-modal-backdrop">
+        <div class="modal details-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">{{ cardName }}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"
+                            v-on:click="closeDetails">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-sm">
                             <div class="form-group">
-                                <h5 class="title">Aktywność</h5>
-                                <Detail class="form-group details-group" v-for="detail in detailsList"
-                                        v-bind:key="detail.id"
-                                        v-bind:content="detail.content" v-bind:date="detail.addDate"
-                                        v-bind:user="detail.userName"/>
+                                <h5 class="title">Listy zadań</h5>
                                 <div class="row">
                                     <div class="col-10">
-                                        <input type="text" class="form-control input-style" id="comment-label"
-                                               placeholder="Napisz komentarz..." v-model="comment">
+                                        <input v-model="newListName" type="text" class="form-control input-style"
+                                               placeholder="Dodaj nową listę zadań...">
                                     </div>
                                     <div class="col-0">
-                                        <i class="material-icons task-icon icon" v-on:click="addComment">send</i>
+                                        <button type="button" v-on:click="addNewTaskList"
+                                                class="btn btn-info btn-sm my-button">Dodaj
+                                        </button>
                                     </div>
                                 </div>
                             </div>
-                        </form>
-                    </div>
-                    <div class="col-sm">
-                        <div class="form-group">
-                            <h5 class="title">Załączniki</h5>
-                            <div class="row">
-                                <div class="col-10">
-                                    <input type="file" ref="file" v-on:change="handleAddAttachment"/>
-                                </div>
-                                <div class="col-0">
-                                    <button type="button" v-on:click="parseAttachment"
-                                            class="btn btn-info btn-sm my-button">Dodaj
-                                    </button>
-                                </div>
+                            <div class="form-group">
+                                <TasksList class="form-group" v-for="taskList in taskLists" v-bind:key="taskList.id"
+                                           v-bind:id="taskList.id"
+                                           v-bind:name="taskList.name" v-bind:taskJsons="taskList.taskJsons"
+                                           @refresh-window="downloadTasks"></TasksList>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <Attachment class="form-group" v-for="attachment in attachments"
-                                        v-bind:key="attachment.id" v-bind:id="attachment.id"
-                                        v-bind:attachment="attachment.attachment"
-                                        v-bind:name="attachment.name"></Attachment>
+                        <div class="col-sm">
+                            <form id="comment-form">
+                                <div class="form-group">
+                                    <h5 class="title">Aktywność</h5>
+                                    <Detail class="form-group details-group" v-for="detail in detailsList"
+                                            v-bind:key="detail.id"
+                                            v-bind:content="detail.content" v-bind:date="detail.addDate"
+                                            v-bind:user="detail.userName"/>
+                                    <div class="row">
+                                        <div class="col-10">
+                                            <input type="text" class="form-control input-style" id="comment-label"
+                                                   placeholder="Napisz komentarz..." v-model="comment">
+                                        </div>
+                                        <div class="col-0">
+                                            <i class="material-icons task-icon icon" v-on:click="addComment">send</i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-sm">
+                            <div class="form-group">
+                                <h5 class="title">Załączniki</h5>
+                                <div class="row">
+                                    <div class="col-10">
+                                        <input type="file" ref="file" v-on:change="handleAddAttachment"/>
+                                    </div>
+                                    <div class="col-0">
+                                        <button type="button" v-on:click="parseAttachment"
+                                                class="btn btn-info btn-sm my-button">Dodaj
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <Attachment class="form-group" v-for="attachment in attachments"
+                                            v-bind:key="attachment.id" v-bind:id="attachment.id"
+                                            v-bind:attachment="attachment.attachment"
+                                            v-bind:name="attachment.name"></Attachment>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -221,6 +224,26 @@
     .icon {
         color: #17A2B8;
         cursor: pointer;
+    }
+
+    .my-modal-backdrop {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: rgba(0, 0, 0, 0.3);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .my-modal {
+        background: #FFFFFF;
+        box-shadow: 2px 2px 20px 1px;
+        overflow-x: auto;
+        display: flex;
+        flex-direction: column;
     }
 
 </style>
